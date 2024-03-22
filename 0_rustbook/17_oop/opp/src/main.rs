47ee049eb143791229569df22e2cc55e78d0177a
Carlos Ramirez <carlos@laranix.com>
2024-03-21 18:17:40 -0600

    add traits duck typing example

---
+++ 0_rustbook/17_oop/opp/src/main.rs
@@ -0,0 +1,49 @@
+use opp::{Draw, Screen};
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("button draw")
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("select draw")
+    }
+}
+
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
