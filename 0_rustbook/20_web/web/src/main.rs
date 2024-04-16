66e00aa4c9f38dc313e49a10e974a3ae492e8895
Carlos Ramirez <carlos@laranix.com>
2024-04-15 20:50:11 -0600

    init threaded pool

--- 0_rustbook/20_web/web/src/main.rs
+++ 0_rustbook/20_web/web/src/main.rs
@@ -1,9 +1,12 @@
-use std::fs;
+use std::{fs, thread};
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use web::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3030").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -17,10 +20,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     let http_request = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename ) = if http_request == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename ) = match &http_request[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
