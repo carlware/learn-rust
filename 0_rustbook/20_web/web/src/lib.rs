3635937db718854da56692f336f1b1b9a391ba92
Carlos Ramirez <carlos@laranix.com>
2024-04-16 18:44:14 -0600

    implement threaded pool

--- 0_rustbook/20_web/web/src/lib.rs
+++ 0_rustbook/20_web/web/src/lib.rs
@@ -1,14 +1,59 @@
-pub struct ThreadPool;
+use std::sync::{Arc, mpsc, Mutex};
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+        let mut threads = Vec::with_capacity(size);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            threads.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            threads,
+            sender
+        }
     }
 
     pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
+        where
+            F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
 
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job; executing.");
+            job();
+        });
+        return Worker{id, thread}
     }
 }
--- 0_rustbook/20_web/web/src/main.rs
+++ 0_rustbook/20_web/web/src/main.rs
@@ -11,7 +11,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
